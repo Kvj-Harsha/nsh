4f53a4ac64c158d273530bd23c0efd2b35574b13
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-08-20 20:19:12 +0530

    changes

--- app/iss/page.js
+++ app/iss/page.js
@@ -1,15 +1,18 @@
 // src/app/page.js
-"use client"
+"use client";
+
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import Navbar2 from '@/components/Navbar2';
 import ISSCoordinates from '@/components/ISSCoordinates';
-import ISSMap from '@/components/ISSMap';
 import { fetchISSLocation } from '@/utils/fetchISSLocation';
 import Features from '@/components/Features';
 import Footer from '@/components/Footer';
 import Redireect from '@/components/Redireect';
 
-
+const ISSMap = dynamic(() => import('@/components/ISSMap'), {
+  ssr: false, // Disable server-side rendering
+});
 
 export default function Home() {
   const [coordinates, setCoordinates] = useState({ latitude: 0, longitude: 0 });
@@ -28,12 +31,12 @@ export default function Home() {
 
   return (
     <div className='bg-[#111827] text-white'>
-      <Navbar2/>
+      <Navbar2 />
       <ISSCoordinates latitude={coordinates.latitude} longitude={coordinates.longitude} />
       <ISSMap latitude={coordinates.latitude} longitude={coordinates.longitude} />
-      <Features/>
-      <Redireect/>
-      <Footer/>
+      <Features />
+      <Redireect />
+      <Footer />
     </div>
   );
 }
